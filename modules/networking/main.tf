ff587babb6133d5cbda6594d6cee474f43439221
kamblekiran <kamblekiran786@gmail.com>
2021-07-22 09:18:39 +0530

    Update main.tf
    Changes done As per my project

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -7,8 +7,8 @@ module "vpc" {
   name                             = "${var.namespace}-vpc"
   cidr                             = "10.0.0.0/16"
   azs                              = data.aws_availability_zones.available.names
-  private_subnets                  = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets                   = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets                  = ["10.0.1.0/24"]
+  public_subnets                   = ["10.0.0.0/24"]
   #assign_generated_ipv6_cidr_block = true
   create_database_subnet_group     = true
   enable_nat_gateway               = true
@@ -23,8 +23,8 @@ resource "aws_security_group" "allow_ssh_pub" {
 
   ingress {
     description = "SSH from the internet"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -49,8 +49,8 @@ resource "aws_security_group" "allow_ssh_priv" {
 
   ingress {
     description = "SSH only from internal VPC clients"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
